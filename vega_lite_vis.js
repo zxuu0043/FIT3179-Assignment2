3d4adc546f6214e037190eb62fb6955acf2fbd90
zxuu0043 <92815695+zxuu0043@users.noreply.github.com>
2021-10-21 17:57:42 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,14 @@
+
+    var specVis1 = "Global COVID-19 total as 10 October 2021.vega";
+    var specVis2= "Daily new cases in Australian states from Sep 10th to Oct 10th.vega";
+    var specVis3= "pie chart.vega";
+  
+    vegaEmbed('#vis1', specVis1, {"actions": false});
+    
+     vegaEmbed('#vis2', specVis2, {"actions": false}).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+  
+    vegaEmbed('#vis3', specVis3, {"actions": false}).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
