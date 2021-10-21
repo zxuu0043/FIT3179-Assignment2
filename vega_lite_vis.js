50be82d977338610a99a2aeaa11f2932291dd4c9
zxuu0043 <92815695+zxuu0043@users.noreply.github.com>
2021-10-21 18:06:10 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,5 @@
 
-    var specVis1 = "Global COVID-19 total as 10 October 2021.vega";
+    var specVis1 = "https://raw.githubusercontent.com//zxuu0043/FIT3179-Assignment2/main/js/Global%20COVID-19%20total%20as%2010%20October%202021.vega";
     var specVis2= "Daily new cases in Australian states from Sep 10th to Oct 10th.vega";
     var specVis3= "pie chart.vega";
   
